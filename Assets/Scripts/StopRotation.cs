8ee0c7695efe5415fdb61d9f84fcd475559b6cc3
naojitaniguchi <naoji.taniguchi@gmail.com>
2020-12-19 20:57:00 +0900

    First commit.

---
+++ Assets/Scripts/StopRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class StopRotation : MonoBehaviour
+{
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<NavMeshAgent>().updateRotation = false;
+        gameObject.GetComponent<NavMeshAgent>().updateUpAxis = false;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<NavMeshAgent>().SetDestination(target.transform.position);
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# 2DNavMesh
+[![youtube](https://img.youtube.com/vi/RTSDmKA02us/0.jpg)](http://www.youtube.com/watch?v=RTSDmKA02us "2DNavMesh")
+
+I'm using MavMeshPlus in this project.  
+https://github.com/h8man/NavMeshPlus
